535868333a58b985f3458cae3f6c6541815afe29
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-10-12 21:00:22 +0100

    Fix errors in js

--- usingclasses.js
+++ usingclasses.js
@@ -11,33 +11,33 @@
 // }
 
 // Add Data to local storage
-class store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-    return books;
-  }
+// class store {
+//   static getBooks() {
+//     let books;
+//     if (localStorage.getItem('books') === null) {
+//       books = [];
+//     } else {
+//       books = JSON.parse(localStorage.getItem('books'));
+//     }
+//     return books;
+//   }
 
-  static addBook(book) {
-    const books = store.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
+//   static addBook(book) {
+//     const books = store.getBooks();
+//     books.push(book);
+//     localStorage.setItem('books', JSON.stringify(books));
+//   }
 
-  static removeBook(id) {
-    const books = store.getBooks();
-    books.forEach((book, index) => {
-      if (book.id.toString() === id) {
-        books.splice(index, 1);
-      }
-    });
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-}
+//   static removeBook(id) {
+//     const books = store.getBooks();
+//     books.forEach((book, index) => {
+//       if (book.id.toString() === id) {
+//         books.splice(index, 1);
+//       }
+//     });
+//     localStorage.setItem('books', JSON.stringify(books));
+//   }
+// }
 
 // // // Add Elements to UI
 // // class UI {
